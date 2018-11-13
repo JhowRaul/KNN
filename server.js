e387756d34b1b6d4073cd11e641a9e7fdd8b9896
Jhow Raul <jhowopet@gmail.com>
2018-11-13 10:42:46 -0200

    Flowers Iris

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ var fs = require('fs'),
     arrayLinha = new Array(),
     minDist,
     arrayD = [],
-    arrayD2 = []
+    arrayD2 = [],
+    confusao = [3[3]]
 
 function singleThread() {
     carrega_matriz = false
-
-
 
     console.log('\n---------------------------------------------')
     console.log('            KNN COM NODE.JS         ')
@@ -30,8 +29,8 @@ function singleThread() {
 
         // Separando arquivo em posições = linhas
         var linhas = data.split(/\r?\n/)
-        p = 1;
-        i = 1;
+        p = 0;
+        i = 0;
         var soma = 0;
 
         linhas.forEach(function (linha) {
@@ -63,39 +62,129 @@ function singleThread() {
 
         })
 
-        // Imprime matriz
-        //console.log(meu_array)
 
-        var contador = 1;
+
+        // Confusao [1][n] = Setosa
+        // Confusao [2][n] = Versicolor
+        // Confusao [3][n] = Virginica
+
+        var tempConfusao = [0, 0, 0, 0];
+        var tempConfusao2 = [0, 0, 0, 0];
+        var tempConfusao3 = [0, 0, 0, 0];
+
+        confusao[1] = tempConfusao;
+        confusao[2] = tempConfusao2;
+        confusao[3] = tempConfusao3;
+
         matriz.forEach(function (item) {
-            if (matriz[contador + 1][contador] != undefined) {
-                //console.log(item)
 
-                matriz.forEach(function (linha_interna) {
+            // Imprime matriz
+            //console.log(item)
+
+            minDist = 999;
+
+
+            matriz.forEach(function (linha_interna) {
+                if (item != linha_interna) {
+
                     soma = 0;
+
+                    // Imprime matriz (imprime 1x toda a matriz para cada linha da mesma) 
                     //console.log(linha_interna)
-                    
+
+
                     for (var c = 0; c < 4; c++) {
-                        soma = soma + Math.pow(item[c] - linha_interna[c], 2)                        
+                        soma = soma + Math.pow(item[c] - linha_interna[c], 2)
                     }
-                    
+
+
                     var resultI = Math.sqrt(soma)
-                    if(resultI < soma){
-                        minDist = resultI
-                        
-                        arrayD = item
-                        arrayD2 = linha_interna
+                    if (resultI < minDist) {
+
+                        minDist = resultI;
+
+
+                        arrayD = item;
+                        arrayD2 = linha_interna;
                     }
-                });
-                
+
+                    //console.log("\n ArrayD Atual: " + arrayD[4]);
+                    //console.log(" ArrayD2 Atual: " +  arrayD2[4]);
+
+
+
+                }
+            });
+
+            //console.log("\n")
+            //console.log(posicao)
+            //console.log(minDist)
+
+            //console.log("\n ArrayD Atual: " + arrayD)
+            //console.log(" ArrayD2 Atual: " + arrayD2)
+
+            // Porque Sempre da iris-virginica?
+
+
+            if (arrayD[4] == "Iris-setosa") {
+                //console.log("\n" + arrayD[4] + "\n" + arrayD2[4])
+                switch (arrayD2[4]) {
+                    case "Iris-setosa":
+                        confusao[1][1] = confusao[1][1] + 1;
+                        break;
+                    case "Iris-versicolor":
+                        confusao[1][2] = confusao[1][2] + 1;
+                        break;
+                    case "Iris-virginica":
+                        confusao[1][3] = confusao[1][3] + 1;
+                        break;
+                    default:
+                        console.log("Falha no switch para setosa")
+                }
             }
+
+            if (arrayD[4] == "Iris-versicolor") {
+                //console.log("\n" + arrayD[4] + "\n" + arrayD2[4])
+                switch (arrayD2[4]) {
+                    case "Iris-setosa":
+                        confusao[2][1] = confusao[2][1] + 1;
+                        break;
+                    case "Iris-versicolor":
+                        confusao[2][2] = confusao[2][2] + 1;
+                        break;
+                    case "Iris-virginica":
+                        confusao[2][3] = confusao[2][3] + 1;
+                        break;
+                    default:
+                        console.log("Falha no switch para versicolor")
+                }
+            }
+
+            if (arrayD[4] == "Iris-virginica") {
+                //console.log("\n" + arrayD[4] + "\n" + arrayD2[4])
+                switch (arrayD2[4]) {
+                    case "Iris-setosa":
+                        confusao[3][1] = confusao[3][1] + 1;
+                        break;
+                    case "Iris-versicolor":
+                        confusao[3][2] = confusao[3][2] + 1;
+                        break;
+                    case "Iris-virginica":
+                        confusao[3][3] = confusao[3][3] + 1;
+                        break;
+                    default:
+                        console.log("Falha no switch para virginica")
+                }
+            }
+
         })
-        
-        
-        console.log(minDist)
-        console.log(arrayD)
-        console.log(arrayD2)
-        
+
+        console.log("-------------- Matriz confusão --------------");
+        console.log("                   "+confusao[1][1]+" "+confusao[1][2]+" "+confusao[1][3]);
+        console.log("                   "+confusao[2][1]+" "+confusao[2][2]+" "+confusao[2][3]);
+        console.log("                   "+confusao[3][1]+" "+confusao[3][2]+" "+confusao[3][3]);
+
+
     }
 
 
